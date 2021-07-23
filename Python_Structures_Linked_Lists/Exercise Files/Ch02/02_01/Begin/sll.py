fce7511c74ce8f98cb9aeada294831e304ddd33b
Sadiq Pasha <86211117+sadpasmgp@users.noreply.github.com>
2021-07-24 01:16:02 +0530

    Python structures Linkedlists

---
+++ Python_Structures_Linked_Lists/Exercise Files/Ch02/02_01/Begin/sll.py
@@ -0,0 +1,25 @@
+class  SLLNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return f"SLLNode: data={self.data}"
+
+    def get_data(self):
+        """Return the self.data attribute"""
+        return self.data
+
+    def set_data(self, new_data):
+        """Replace the existsing value of the self.data attribute with new_data"""
+        self.data = new_data
+
+    def get_next(self):
+        """Return the self.next attribute"""
+        return self.next
+
+    def set_next(self, new_next):
+        """Replace the existing value of the self.next attribute with new_next"""
+        self.next = new_next
+
